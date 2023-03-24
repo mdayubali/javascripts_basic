669bc2de9758f60a4ba1bb99b3683f37d5adad2e
mdayubali <ayubh9288@gmail.com>
2023-03-24 17:05:15 +0600

    slice() and splice() comments update

--- sliceSplice.js
+++ sliceSplice.js
@@ -7,7 +7,9 @@ console.log(elements)
 
 // splice will create a new array and these will be delete from the main array as well as the array will be change
 // splice work with start point and how many index/element you want to remove or create a new array using splice
+// also you can add new elements after count number in the original array at the deleted placed
 
-const newItems = elements.splice(3,8)
+const newItems = elements.splice(3,4,1,2,3,4,5)
+// the above example new array will create from index 3 with 4 number and add the rest element
 console.log(newElement)
 console.log(elements)
