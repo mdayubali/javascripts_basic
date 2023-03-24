7a1dbc459640701104644d6025c48044501e4aef
mdayubali <ayubh9288@gmail.com>
2023-03-24 17:24:56 +0600

    remove duplicate items from an array

---
+++ removeduplicatearray.js
@@ -0,0 +1,14 @@
+const names = ['babul','abul','kabul','malu','kalu','abul','babul','abul','kabul','jalu','peru']
+
+function removeDuplicate(names){
+    const unique = [];
+    for(let i = 0; i<names.length; i++){
+        const name = names[i];
+        if(unique.includes(name) === false){
+            unique.push(name);
+        }
+    }
+    return unique;
+}
+const result = removeDuplicate(names)
+console.log(result)
