b48d475fe559b31bfbafcac74d3cc407b1a52eaa
mdayubali <ayubh9288@gmail.com>
2023-04-07 15:44:07 +0600

    product cart calculation

---
+++ cartupdate.js
@@ -0,0 +1,17 @@
+const products = [
+    {name: 'shirt', price: 1200, quantity: 2},
+    {name: 'pent', price: 1700, quantity: 5},
+    {name: 'shoe', price: 2800, quantity: 3},
+    {name: 'belt', price: 700, quantity: 4},
+]
+function totalPrice(totalProduct){
+    let sum = 0;
+    for(let i = 0; i<products.length; i++){
+        const product = products[i];
+        const  totalCost = product.price * product.quantity
+        sum = sum + totalCost 
+    }
+    return sum;
+}
+const expense = totalPrice(products)
+console.log(expense)
